de12bcd13fe4fa9f18b8fb1c593dca7a7a48a0da
svturkin <70198600+svturkin@users.noreply.github.com>
2020-11-03 16:55:12 +0300

    Add files via upload

---
+++ model.js
@@ -0,0 +1,45 @@
+import image from './assets/image.png'
+import {TextBlock, TitleBlock, ColumnsBlock, ImageBlock} from './classes/blocks'
+
+const text = `Turkin Stanislav`
+
+export const model = [
+    new TitleBlock('Site Constructor', {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center'
+        }
+    }),
+    new ImageBlock(image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '500px',
+            height: 'auto'
+        },
+        alt: 'Это картинка'
+    }),
+    new ColumnsBlock([
+        'Приложение на чистом JavaScript, без использования библиотек'
+    ], {
+        styles: {
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+            padding: '2rem',
+            color: '#fff',
+            'font-weight': 'bold'
+        }
+    }),
+    new TextBlock(text, {
+        styles: {
+            background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+            padding: '1rem',
+            'font-weight': 'bold'
+        }
+    })
+]
